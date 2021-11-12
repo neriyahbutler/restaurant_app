d77d2d91c7419be20a6f54552bc8cd316e8d9dfb
gracedoughh <gracetdo@gmail.com>
2021-11-11 18:05:16 -0600

    signin frontend

--- frontend/src/components/Signin/Signin.js
+++ frontend/src/components/Signin/Signin.js
@@ -1,13 +1,53 @@
-import React from 'react';
+import React, { useState }  from 'react';
 import './Signin.css';
 
+//Signin Box
 class Signin extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            isSigninOpen: true,
+            isRegisterOpen: false
+        };
+    }
+
+    submitSignin(e) {}
+    
     render() {
         return (
-            <div>
-                <h1>Sign In</h1>
+            <div className = "inner-container">
+                <div className = "header">
+                    <h1> Sign In</h1>
+                </div>
+                
+                <div className = "box">
+                    <div className = "input-group" >
+                        <label htmlFor = "username">Username</label>
+                        <input
+                            type = "text"
+                            name = "username"
+                            className = "login-input"
+                            placeholder = "Username" />
+                    </div>
+
+                    <div className = "input-group">
+                        <label htmlFor = "password">Password</label>
+                        <input
+                            type = "text"
+                            name = "username"
+                            className = "login-input"
+                            placeholder = "Password" />
+                    </div>
+
+                    <button
+                        type = "button"
+                        className = "login-btn"
+                        onClick = {this
+                        .submitSignin
+                        .bind(this)}>Login</button>
+                </div>
             </div>
-        )
+        );
     }
 }
 
